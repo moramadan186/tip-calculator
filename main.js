97c5a3183999a1090e0df4a6c6659f5843c885dc
mohamed ramadan <mr01028760097@gmail.com>
2021-06-24 18:03:28 +0200

    add some features

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-var theResult=document.getElementById("result");
+var theResult = document.getElementById("result");
 
-  var selectBox =document.querySelector("select"),
-  option=document.querySelectorAll("option");
+var selectBox = document.querySelector("select"),
+  option = document.querySelectorAll("option");
 
-function calculateTip(x,y){
-  var reso1=(x*y)/100;
-  var peopleValue=document.getElementById("people").value;
-  var reso2=reso1/(peopleValue);
-theResult.textContent=reso2;
+function calculateTip(x, y) {
+  var reso1 = (x * y) / 100;
+  var peopleValue = document.getElementById("people").value;
+  var reso2 = reso1 / (peopleValue);
+  theResult.textContent = `${reso2} $ you can pay it`;
 }
 
 var percentg;
-selectBox.onchange = function(){
-  percentg=selectBox.value;
-   // for(i=0;i<option.length;i++){
-   // if(selectBox.value==option[i].value){
-   //   theResult.textContent=option[i].textContent;
-   // }
-   // }
-   
+selectBox.onchange = function () {
+  percentg = selectBox.value;
+  // for(i=0;i<option.length;i++){
+  // if(selectBox.value==option[i].value){
+  //   theResult.textContent=option[i].textContent;
+  // }
+  // }
+
 }
- 
- function finalCalc() {
-    var billValue=document.getElementById("dollars").value;
-  calculateTip(billValue,percentg);
+
+function finalCalc() {
+  var billValue = document.getElementById("dollars").value;
+  calculateTip(billValue, percentg);
 }
 
 
